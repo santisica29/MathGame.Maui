aa320194f771f5f0fded48fbf2f393d1dcf53f96
Santiago Sica <santisica29@gmail.com>
2026-01-09 12:13:24 -0300

    Add project files.

---
+++ MathGame.Maui/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MathGame.Maui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MathGame.Maui.Pages")]
