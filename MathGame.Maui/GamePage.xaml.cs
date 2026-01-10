b17778c8535f5307badaa48a12436d125828e730
Santiago Sica <santisica29@gmail.com>
2026-01-10 00:17:57 -0300

    Initial game logic

---
+++ MathGame.Maui/GamePage.xaml.cs
@@ -0,0 +1,68 @@
+namespace MathGame.Maui;
+
+public partial class GamePage : ContentPage
+{
+	public string GameType { get; set; }
+	int firstNumber = 0;
+	int secondNumber = 0;
+	public GamePage(string gameType)
+	{
+		InitializeComponent();
+		GameType = gameType;
+		BindingContext = this;
+
+		CreateNewQuestion();
+	}
+
+	private void CreateNewQuestion()
+	{
+		var gameOperand = GameType switch
+		{
+			"Addition" => "+",
+			"Subtraction" => "-",
+			"Multiplication" => "*",
+			"Division" => "/",
+			_ => ""
+		};
+
+		var random = new Random();
+
+		firstNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+        secondNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+
+		if (GameType == "Division")
+			while (firstNumber < secondNumber || firstNumber % secondNumber != 0)
+				firstNumber = random.Next(1, 99);
+				secondNumber = random.Next(1, 99);
+
+
+		QuestionLabel.Text = $"{firstNumber} {gameOperand} {secondNumber}?";
+
+    }
+
+	private void OnAnswerSubmitted(object sender, EventArgs e)
+	{
+		var answer = Int32.Parse(AnswerEntry.Text);
+		var isCorrect = false;
+
+		switch (GameType)
+		{
+			case "Addition":
+				isCorrect = answer == firstNumber + secondNumber;
+				break;
+			case "Subtraction":
+				isCorrect = answer == firstNumber - secondNumber;
+				break;
+			case "Multiplication":
+				isCorrect = answer == firstNumber * secondNumber;
+				break;
+			case "Division":
+				isCorrect = answer == firstNumber / secondNumber;
+				break;
+			default:
+				break;
+		}
+
+
+	}
+}
--- MathGame.Maui/MainPage.xaml.cs
+++ MathGame.Maui/MainPage.xaml.cs
@@ -6,5 +6,19 @@
         {
             InitializeComponent();
         }
+
+        private void OnGameChosen(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            Navigation.PushAsync(new GamePage(btn.Text));
+        }
+
+        private void OnViewPreviousGamesChosen(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new PreviousGames());
+
+        }
     }
 }
